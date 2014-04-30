a9eec28c2c8a5d53098d608f63fa102e7c8f3eb7
anthony <clintanthony2492@yahoo.com>
2014-04-30 11:06:56 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ int answer=0;
 
 	answer = 5 + 5;
 	cout << "5 + 5=" << answer;
+	
+	//Person 2: Anthony updates
+	//---------------------------
+	int answer2 = 0;
+	answer2 = 5 * 5;
+	cout << "5 * 5= " << answer2;
+	//---------------------------
+	
 
 return 0;
 }
